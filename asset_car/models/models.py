5deb78c29059f86b56ce59ea42d0436e2dd617e0
mohmed0007 <104038330+mohmed0007@users.noreply.github.com>
2022-09-11 17:07:25 +0300

    Add files via upload

--- asset_car/models/models.py
+++ asset_car/models/models.py
@@ -6,7 +6,7 @@ class AccountAsset(models.Model):
     _inherit = "account.asset"
     
     car_is = fields.Boolean(
-        string='Is Car', default=False
+        string='', default=False
     )
 
     
